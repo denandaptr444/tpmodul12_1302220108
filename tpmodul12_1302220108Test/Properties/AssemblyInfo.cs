fd5fffe9a239b15593ba9bb02577924559b400eb
denan <denan@DESKTOP-C4Q70KR>
2024-05-13 03:10:24 +0700

    Add project files.

---
+++ tpmodul12_1302220108Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("tpmodul12_1302220108Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tpmodul12_1302220108Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df0e84b9-65d5-42cb-8f41-2ac0f7e0d38a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tpmodul12_1302220108Test/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using tpmodul12_1302220108;
+
+namespace tpmodul12_1302220108Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Form1 form = new Form1();
+
+            form.CariTandaBilangan(100);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Form1 form = new Form1();
+
+            form.CariTandaBilangan(-100);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Form1 form = new Form1();
+
+            form.CariTandaBilangan(0);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Form1 form = new Form1();
+            string result = form.CariTandaBilangan(-5);
+            Assert.AreEqual("Positif", result, "Expected 'Positif' for input -5.");
+        }
+    }
+}
